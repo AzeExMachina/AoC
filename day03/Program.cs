ad84c58102cfa6a46656d629dfd8bb5f10ab310d
Aze <aldegheri.d@gmail.com>
2024-12-04 15:03:57 +0100

    day 3 first

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Text.RegularExpressions;
 
 var regex = MyRegex();
+var file = File.ReadAllText("../../../input.txt");
+var matches = regex.Matches(file).Cast<Match>();
+var sum = matches.Select(match => match.ToString().Replace("mul(", string.Empty).Replace(")", string.Empty).Split(',').Select(int.Parse)).Select(factors => factors.First() * factors.Last()).Sum();
+Console.WriteLine(sum);
 
 partial class Program
 {
     [GeneratedRegex(@"mul\(\d{1,3},\d{1,3}\)")]
     private static partial Regex MyRegex();
 }
-
