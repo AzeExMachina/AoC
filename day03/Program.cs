3d8e9b09dc9eb09e190ff667b62243562d7a738d
Aze <aldegheri.d@gmail.com>
2024-12-04 13:16:27 +0100

    removed inputs

---
+++ day03/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+var regex = MyRegex();
+
+partial class Program
+{
+    [GeneratedRegex(@"mul\(\d{1,3},\d{1,3}\)")]
+    private static partial Regex MyRegex();
+}
+
